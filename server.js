9d0dc26cf9d6779dd0b66c16d93787eacf8e146e
Bikal <82023507+Bikal-Adhikari@users.noreply.github.com>
2024-07-09 14:31:33 +1000

    Order confirm msg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv";
 import express from "express";
 import Stripe from "stripe";
+import cors from "cors";
 const app = express();
 
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 const stripe = new Stripe(process.env.stripe_SecretKey);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
 app.post("/create-stripe-payments", async (req, res) => {
   try {
     const { amount, currency, paymentMethod } = req.body;
@@ -23,6 +25,24 @@ app.post("/create-stripe-payments", async (req, res) => {
     return res.json({
       success: "false",
       error: "something went wrong",
+    });
+  }
+});
+
+app.post("/confirm-order", (req, res) => {
+  try {
+    console.log(req.body);
+    if (req.body.id) {
+      return res.json({
+        success: "true",
+        message: "Order Saved",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      success: "false",
+      message: "something went wrong",
     });
   }
 });
